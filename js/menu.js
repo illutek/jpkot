11b7ebb13b0ef388a484e40bd2e3adcd04e9ad28
Stefaan Vandenborne <stefanvandenborne@gmail.com>
2017-04-16 21:09:27 +0200

    menu verder uitgewerkt

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,14 @@
  * Created by Stefan on 16/04/2017.
  */
 $(document).ready(function () {
+    $('.menu-right-wrapper').hide();
+
     $('.hamburger-icon').click(function(){
         $('.menu-right-wrapper').toggleClass('menu-right-visible');
+    });
+
+    $('.hamburger-icon').one('click', function(){
+        $('.menu-right-wrapper').show();
     });
 
     $('.close-btn').click(function(){
