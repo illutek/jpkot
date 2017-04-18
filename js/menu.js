61bf07e98bf4ad8204f9996b5089c0c0ee8401f8
Stefaan Vandenborne <stefanvandenborne@gmail.com>
2017-04-18 14:51:48 +0200

    menu verder layout

--- js/menu.js
+++ js/menu.js
@@ -2,20 +2,18 @@
  * Created by Stefan on 16/04/2017.
  */
 $(document).ready(function () {
-    $('.menu-right-wrapper').hide();
 
-    $('.hamburger-icon').click(function(){
-        $('.menu-right-wrapper').toggleClass('menu-right-visible');
-    });
 
-    $('.hamburger-icon').one('click', function(){
-        $('.menu-right-wrapper').show();
+    $('.menu-wrap').click(function(){
+        $('.menu-right-wrapper').toggleClass('menu-right__visible');
+        $('.menu-wrap').toggleClass('menu-wrap__visible');
     });
 
     $('.close-btn').click(function(){
-        $('.menu-right-wrapper').toggleClass('menu-right-visible');
-        $('.hamburger-icon').show();
+        $('.menu-right-wrapper').toggleClass('menu-right__visible');
+        $('.menu-wrap').toggleClass('menu-wrap__visible');
     });
+
 });
 
 
