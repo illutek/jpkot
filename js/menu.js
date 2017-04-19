916d27075cba97d0b0a1201a92911e430c782978
stefan <stefan@illutek.be>
2017-04-19 21:03:06 +0200

    bug jquery load
    extra function($) wrap in custom js

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,17 @@
 /**
  * Created by Stefan on 16/04/2017.
  */
-$(document).ready(function () {
+(function ($){
+    $(document).ready(function () {
 
+        $('.menu-wrap').click(function () {
+            $('.menu-right-wrapper').toggleClass('menu-right__visible');
+            $('.menu-wrap').toggleClass('menu-wrap__visible');
+        });
 
-    $('.menu-wrap').click(function(){
-        $('.menu-right-wrapper').toggleClass('menu-right__visible');
-        $('.menu-wrap').toggleClass('menu-wrap__visible');
+        $('.close-btn').click(function () {
+            $('.menu-right-wrapper').toggleClass('menu-right__visible');
+            $('.menu-wrap').toggleClass('menu-wrap__visible');
+        });
     });
-
-    $('.close-btn').click(function(){
-        $('.menu-right-wrapper').toggleClass('menu-right__visible');
-        $('.menu-wrap').toggleClass('menu-wrap__visible');
-    });
-
-});
-
-
-
+})(jQuery);
