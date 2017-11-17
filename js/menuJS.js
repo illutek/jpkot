051435bec600506ebd6d8e50b7d83718d991dfc5
Stefan Vandenborne <stefan@localhost.localdomain>
2017-11-17 21:46:03 +0100

    adding JS DOM

---
+++ js/menuJS.js
@@ -0,0 +1,9 @@
+// const menuRightWrap = document.getElementsByClassName('menu-right-wrapper');
+// const menuWrap = document.getElementsByClassName('menu-wrap');
+
+document.getElementsByClassName("menu-wrap").attachEvent('onclick', test);
+
+function test() {
+
+    console.log("heeft geklikt");
+}
