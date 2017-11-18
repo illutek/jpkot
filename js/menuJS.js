9adbe8139164c3c92ee34e46e07d61f871242be9
Vandenborne Stefan <stefan@illutek.be>
2017-11-18 11:18:10 +0100

    JS-menu works

--- js/menuJS.js
+++ js/menuJS.js
@@ -1,9 +1,34 @@
-// const menuRightWrap = document.getElementsByClassName('menu-right-wrapper');
-// const menuWrap = document.getElementsByClassName('menu-wrap');
+/**
+ * Created by Stefan on 17/11/2017.
+ * Strict mode makes it easier to write "secure" JavaScript.
+ */
+(function () {
+    'use strict';
 
-document.getElementsByClassName("menu-wrap").attachEvent('onclick', test);
+    var openBtnMenu = document.getElementsByClassName('menu-wrap')[0];
+    var menuRightWrapper = document.getElementsByClassName('menu-right-wrapper')[0];
+    var containerWrapper = document.getElementsByClassName('container__wrapper')[0];
+    var closeBtn = document.getElementsByClassName('close-btn')[0];
 
-function test() {
 
-    console.log("heeft geklikt");
-}
+    function closeMenu() {
+        menuRightWrapper.classList.remove('menu-right__visible');
+        containerWrapper.classList.remove('pushover-left');
+    }
+
+    function openMenu() {
+        menuRightWrapper.classList.toggle('menu-right__visible');
+        containerWrapper.classList.toggle('pushover-left');
+    }
+
+    closeMenu()
+
+    closeBtn.addEventListener('click', function () {
+        closeMenu()
+    });
+
+    openBtnMenu.addEventListener('click', function () {
+        openMenu()
+    });
+
+}());
